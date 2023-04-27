d30ffaec33fd988e89e2de7dbdbae382ccbd7929
Harvansh S Rathore <78679500+Harvansh-1234@users.noreply.github.com>
2023-04-28 03:19:05 +0530

    proceed modal QR scan

--- screens/ScanScreen.js
+++ screens/ScanScreen.js
@@ -1,10 +1,51 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Button} from 'react-native';
+import { StyleSheet, Text, Button, View, Modal, Image, TouchableOpacity, Animated, } from 'react-native';
 import { BarCodeScanner } from "expo-barcode-scanner";
+import { Ionicons } from '@expo/vector-icons';
+
+
+const ModalPoup = ({ visible, children }) => {
+  const [showModal, setShowModal] = React.useState(visible);
+  const scaleValue = React.useRef(new Animated.Value(0)).current;
+  React.useEffect(() => {
+    toggleModal();
+  }, [visible]);
+  const toggleModal = () => {
+    if (visible) {
+      setShowModal(true);
+      Animated.spring(scaleValue, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      setTimeout(() => setShowModal(false), 200);
+      Animated.timing(scaleValue, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    }
+  };
+  return (
+    <Modal transparent visible={showModal}>
+      <View style={styles.modalBackGround}>
+        <Animated.View
+          style={[styles.modalContainer, { transform: [{ scale: scaleValue }] }]}>
+          {children}
+        </Animated.View>
+      </View>
+    </Modal>
+  );
+};
+
+
 const ScanScreen = () => {
+  const [visible, setVisible] = useState(false);
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
   const [text, setText] = useState("Not yet scanned");
+
 
   const askForCameraPermission = () => {
     (async () => {
@@ -49,6 +90,26 @@ const ScanScreen = () => {
   return (
 
     <View style={styles.container}>
+      <ModalPoup visible={visible}>
+        <View style={{alignItems: 'center'}}>
+          <View style={styles.header}>
+          <TouchableOpacity onPress={() => setVisible(false)}>
+              {/* <Image
+                source={require('../assets/m1.png')}
+                style={{height: 30, width: 30}}
+              /> */}
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={{alignItems: 'center'}}>
+          <Image
+            source={require('../assets/m2.png')}
+            style={{height: 150, width: 150, marginVertical: 10}}
+          />
+        </View>
+        <Button title="Submit" onPress={()=>{}} />
+      </ModalPoup>
+
       <View style={styles.barcodebox}>
         <BarCodeScanner
           onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
@@ -67,11 +128,12 @@ const ScanScreen = () => {
       {scanned && (
         <Button
           title={"Proceed"}
-          onPress={handlePresentModalPress}
-           color="black"
+          onPress={() => setVisible(true)} 
+          color="black"
         />
       )}
     </View>
+
   );
 };
 
@@ -94,6 +156,26 @@ const styles = StyleSheet.create({
     overflow: "hidden",
     borderRadius: 30,
     backgroundColor: "tomato",
+  },
+  modalBackGround: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContainer: {
+    width: '80%',
+    backgroundColor: 'white',
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    borderRadius: 20,
+    elevation: 20,
+  },
+  header: {
+    width: '100%',
+    height: 40,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
   },
 });
 
